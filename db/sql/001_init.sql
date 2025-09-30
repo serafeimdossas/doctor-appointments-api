1a48101e4fcb196fe6a8a8431d2c730a1fd890f7
Serafeim Dossas <serafeimdossas@gmail.com>
2025-09-30 15:28:32 +0300

    init sql file change

--- db/sql/001_init.sql
+++ db/sql/001_init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS doctor (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     specialty VARCHAR(100) NOT NULL
-    address VARCHAR(255) NOT NULL,
+    office_address VARCHAR(255) NOT NULL,
     phone VARCHAR(15) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 )
